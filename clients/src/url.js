a5b2686a7a159fc4ea5a64192de084e0bc4e786b
Sanchay <sanchaynts9422@gmail.com>
2024-08-16 04:36:23 +0530

    couldnt deploy

--- clients/src/url.js
+++ clients/src/url.js
@@ -1,2 +1,2 @@
-// export const URL = "http://localhost:8000";
-export const URL = "https://gym-backend-ucnw.onrender.com";
+export const URL = "http://localhost:8000";
+// export const URL = "https://gym-backend-ucnw.onrender.com";
