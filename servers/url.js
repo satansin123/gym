1b4d8cd0bf3b7f2c8c4f2078506e12a6df57b6fe
Sanchay <sanchaynts9422@gmail.com>
2024-08-16 04:31:17 +0530

    deploy

--- servers/url.js
+++ servers/url.js
@@ -1,2 +1,6 @@
-// export const URL = "http://localhost:3000";
-export const URL = "https://gym-4dsc.onrender.com";
+module.exports = {
+  URL: "https://gym-4dsc.onrender.com",
+};
+//   module.exports = {
+//     URL: "http://localhost:3000"
+//   };
